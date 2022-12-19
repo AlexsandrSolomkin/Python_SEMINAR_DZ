eff09467848460bfa5c1af112c3d2749a80a6ccf
Solomkin Alexandr <solomin1995@mail.ru>
2022-12-20 01:03:37 +0300

    DZ_2_5: completing task DZ_16.py

--- Python_DZ_SEMINAR_3/DZ_16.py
+++ Python_DZ_SEMINAR_3/DZ_16.py
@@ -10,10 +10,54 @@
 # 1 2 3 4 5
 # Вывод: 1
 
-#========================================================================
+# ========================================================================
 
-#Решение:
+# Решение:
+
+num = int(input("Ведите N размер массива, который вы хотите заполнить: "))
+хNum = int(input("Нужное число, которое может быть от 1 до N/2: "))
+
+numMax = int(num / 2)
 
 
+def GetList(n, nMax):
 
-#========================================================================
+    import random
+
+    l = []
+
+    for i in range(n):
+
+        random_number = round(random.randint(1, nMax))
+        l.append(random_number)
+
+    return l
+
+
+def ElemntsXList(x, listElements):
+
+    count = 0
+
+    for g in range(len(listElements)):
+
+        if listElements[g] == x:
+
+            count += 1
+
+    return count
+
+
+newList = result = 0
+
+if numMax >= хNum >= 1:
+
+    newList = GetList(num, numMax)
+    result = ElemntsXList(хNum, newList)
+
+    print(newList, result, sep="\n")
+
+else:
+
+    print("Число некорректно")
+
+# ========================================================================
