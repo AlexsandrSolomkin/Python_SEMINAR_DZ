508f59c78531cb94c270cab05e8fa52ebbf5372e
Solomkin Alexandr <solomin1995@mail.ru>
2022-12-23 18:48:14 +0300

    DZ_20: completing task DZ_20.py

--- Python_DZ_SEMINAR_3/DZ_20.py
+++ Python_DZ_SEMINAR_3/DZ_20.py
@@ -28,6 +28,58 @@
 
 # Решение:
 
+def Module_LanguagePlayer(playerLang):
+
+    d_ENG = \
+        {
+            'A': 1, 'E': 1, 'I': 1, 'O': 1, 'U': 1, 'L': 1, 'N': 1, 'S': 1, 'T': 1, 'R': 1,
+            'D': 2, 'G': 2,
+            'B': 3, 'C': 3, 'M': 3, 'P': 3,
+            'F': 4, 'H': 4, 'V': 4, 'W': 4, 'Y': 4,
+            'K': 5,
+            'J': 8, 'X': 8,
+            'Q': 10, 'Z': 10
+        }
+
+    d_RUS = \
+        {
+            'А': 1, 'В': 1, 'Е': 1, 'И': 1, 'Н': 1, 'О': 1, 'Р': 1, 'С': 1, 'Т': 1,
+            'Д': 2, 'К': 2, 'Л': 2, 'М': 2, 'П': 2, 'У': 2,
+            'Б': 3, 'Г': 3, 'Ё': 3, 'Ь': 3, 'Я': 3,
+            'Й': 4, 'Ы': 4,
+            'Ж': 5, 'З': 5, 'Х': 5, 'Ц': 5, 'Ч': 5,
+            'Ш': 8, 'Э': 8, 'Ю': 8,
+            'Ф': 10, 'Щ': 10, 'Ъ': 10
+        }
+
+    dictionary = {}
+
+    if playerLang == "ENG":
+        dictionary = d_ENG
+    elif playerLang == "RUS":
+        dictionary = d_RUS
+    else:
+        dictionary = "Такого языка нет в базе данных"
+
+    return dictionary
+
+
+def PlayGame(languagePlayer, answer):
+
+    points = 0
+    for i in answer:
+        points += languagePlayer[i]
+
+    return points
+
+
+language = input("Enter your language -> ENG <- or -> RUS <-: ").upper()
+
+langPlayer = Module_LanguagePlayer(language)
+
+playerAnswer = input("Ваш ответ: ").upper()
+
+print("Вы заработали:", PlayGame(langPlayer, playerAnswer), "очков!!!")
 
 
 # ========================================================================
